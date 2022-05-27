bc290e0ddf4c7b62a7733720f27c79109530efd4
MosheWork <88563378+MosheWork@users.noreply.github.com>
2022-05-27 13:34:34 +0300

    add line to model

--- server/models/messageModel.js
+++ server/models/messageModel.js
@@ -1,17 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const messageSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, 'Please add a title.']
+const messageSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please add a title."],
+    },
+    message: {
+      type: String,
+      required: [true, "Please add a message."],
+    },
+    priority: {
+      type: String,
+      required: [true, "Please add a priority."],
+    },
   },
-  message: {
-    type: String,
-    required: [true, 'Please add a message.'],
-  },
-},
-{
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Message', messageSchema);
+module.exports = mongoose.model("Message", messageSchema);
